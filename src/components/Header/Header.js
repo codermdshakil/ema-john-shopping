e4348b02553fd2b20caf8ef03c748a0764ced4f1
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-03-24 02:34:46 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 
 const Header = () => {
     return (
-         <nav>
-            <img src={logo} alt="" />
-            <div className='nav-links'>
-                <a href="/shop">Shop</a>
-                <a href="/order">Order</a>
-                <a href="/orderreviw">Order Review</a>
-                <a href="/manageinventory"> Manege Inventory</a>
-            </div>
-          </nav>
+        <Navbar className="header-nav" expand="lg" fixed="top">
+        <Container>
+            <Navbar.Brand href="#home">
+                <img src={logo} alt="" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse  id="basic-navbar-nav">
+            <Nav className="ms-auto">
+                <li className="nav-item">
+                    <a href="./shop" className="nav-item">Shop</a>
+                </li>
+                <li className="nav-item">
+                    <a href="./order" className="nav-item">Order</a>
+                </li>
+                <li className="nav-item">
+                    <a href="./order-review" className="nav-item">Order Review</a>
+                </li>
+                <li className="nav-item">
+                    <a href="./manage-inventory" className="nav-item">Manage Inventory</a>
+                </li>
+                
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
     );
 };
 
